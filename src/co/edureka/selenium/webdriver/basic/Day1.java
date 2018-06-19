a949ca8a0779f426fc50495c2cc95ec65c60a796
LEPTON\mana <tamaoki_mana@yahoo.co.jp>
2018-06-20 08:02:26 +0900

    1:58:32

--- src/co/edureka/selenium/webdriver/basic/Day1.java
+++ src/co/edureka/selenium/webdriver/basic/Day1.java
@@ -1,6 +1,17 @@
 package co.edureka.selenium.webdriver.basic;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Day1 {
+	
+	WebDriver driver;
+	
+	public void invokeBrowser() {
+		
+		System.setProperty("webdriver.chrome.driver", "E:\\tools\\Selenium\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
