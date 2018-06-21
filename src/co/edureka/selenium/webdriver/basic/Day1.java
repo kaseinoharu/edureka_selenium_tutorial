8a6510033e9fdde3c202f3ece6a69443deb58de5
Mana Tamaoki DESKTOP-LOA7VBD <tamaoki_mana@yahoo.co.jp>
2018-06-22 07:46:58 +0900

    Invoke a Google Chrome browser

--- src/co/edureka/selenium/webdriver/basic/Day1.java
+++ src/co/edureka/selenium/webdriver/basic/Day1.java
@@ -1,5 +1,7 @@
 package co.edureka.selenium.webdriver.basic;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,13 +11,25 @@ public class Day1 {
 	
 	public void invokeBrowser() {
 		
-		System.setProperty("webdriver.chrome.driver", "E:\\tools\\Selenium\\chromedriver_win32\\chromedriver.exe");
-		driver = new ChromeDriver();
+		try {
+			System.setProperty("webdriver.chrome.driver", "D:\\tools\\Selenium\\chromedriver_win32\\chromedriver.exe");
+			driver = new ChromeDriver();
+			driver.manage().deleteAllCookies();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+			
+			driver.get("http://www.edureka.co");
+//			driver.get("http://l-tike.com/");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// てすと用コメント
+		
+		Day1 myObj = new Day1();
+		myObj.invokeBrowser();
 
 	}
 
