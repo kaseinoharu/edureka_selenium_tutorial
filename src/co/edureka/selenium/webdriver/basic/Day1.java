b801aa742116baa9d02c8436fdbd6a1b1883332d
LEPTON\mana <tamaoki_mana@yahoo.co.jp>
2018-06-27 07:33:33 +0900

    using JavascriptExecutor

--- src/co/edureka/selenium/webdriver/basic/Day1.java
+++ src/co/edureka/selenium/webdriver/basic/Day1.java
@@ -3,17 +3,22 @@ package co.edureka.selenium.webdriver.basic;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+
 
 public class Day1 {
 	
 	WebDriver driver;
+	JavascriptExecutor jse;
 	
 	public void invokeBrowser() {
 		
 		try {
-			System.setProperty("webdriver.chrome.driver", "D:\\tools\\Selenium\\chromedriver_win32\\chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver", "E:\\tools\\Selenium\\chromedriver_win32\\chromedriver.exe");
 			driver = new ChromeDriver();
 			driver.manage().deleteAllCookies();
 			driver.manage().window().maximize();
@@ -33,6 +38,12 @@ public class Day1 {
 			driver.findElement(By.id("homeSearchBar")).sendKeys("Java");
 			Thread.sleep(3000);
 			driver.findElement(By.id("homeSearchBarIcon")).click();
+//			jse = (JavascriptExecutor)driver;
+//			jse.executeScript("scroll(0, 10000)");
+			Thread.sleep(3000);
+			driver.findElement(By.id("batch-type-button")).click();
+			Thread.sleep(3000);
+			driver.findElement(By.id("week_end")).click();
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
